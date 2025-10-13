a85cf4e082f305b39e18901f9092a3b608e7ff0a
Masoome006 <masoome.rajabi4851@gmail.com>
2025-10-13 20:59:10 +0330

    Q2 done

---
+++ tamrin/src/main/java/com/example/Q2.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Q2{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        char c = scanner.next().charAt(0);
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n ; j++){
+                if(i==1 ||i==n || j==1 || j==n)
+                    System.out.print(c);                
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(i==j || i+j == n-1)
+                    System.out.print(c);
+                    else
+                        System.out.print(" ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
