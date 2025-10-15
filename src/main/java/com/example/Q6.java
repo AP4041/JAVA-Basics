80c7aa7bc263625f49ca33b2c0d6d32c9b7c1da3
gholihashta-lgtm <gholihashira@gmail.com>
2025-10-15 17:30:12 +0330

    q6 done

--- src/main/java/com/example/Q6.java
+++ src/main/java/com/example/Q6.java
@@ -1,38 +1,48 @@
 package com.example;
+
+
 import java.util.Scanner;
+
 public class Q6 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-    
-Scanner scanner=new Scanner(System.in);
-
-
-int n=scanner.nextInt();
-
-int []array=new int[n];
-for(int i=0;i<n;i++)array[i]=scanner.nextInt();
-
-sort(array);
-
-for(int i=0;i<n;i++)System.out.print(array[i]+"  ");
-
-scanner.close();;
-    }   
-    
-    public static void sort(int[] arr) {
-
-        int n=arr.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if(arr[j]>arr[j+1]){
-
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
-            }
-            
+        int n = scanner.nextInt();
+        
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
         }
         
+        bubbleSort(array);
+ for(int i = 0; i < n; i++)System.out.print(array[i]+" ");
+
+
+        System.err.print("t=");
+        int t = scanner.nextInt();
+        int index = -1;
+        for(int i = 0; i < n; i++) {
+            if(array[i] == t) {
+                index = i;
+                break;
+            }
+        }
+       
+        System.out.println(index+1);
+        
+        scanner.close();
+    }
+    
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
