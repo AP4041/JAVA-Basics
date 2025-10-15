dc6802e2c36247738af7319209ba888db3e16e7a
Neon-Raven-1984 <hakimfranky84@gmail.com>
2025-10-15 22:50:06 +0330

    Q1 Done.

---
+++ tamrin/src/main/java/com/example/Ex.java
@@ -0,0 +1,15 @@
+package com.example;
+import java.util.Scanner;  
+
+public class Ex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        double b = sc.nextDouble();
+        System.out.println(b);
+        String c = sc.next();
+        System.out.println(c);
+        sc.close();
+    }
+}
--- tamrin/src/main/java/com/example/Q1.java
+++ tamrin/src/main/java/com/example/Q1.java
@@ -1,8 +1,57 @@
 package com.example;
+import java.util.Scanner;
+
 
 public class Q1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Im here.");
+
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        for (int i = 0 ; i<n ; i++){
+            System.out.print("Enter element " + i + ": ");
+            array[i] = sc.nextInt();
+        }
+        
+        for (int i = 0 ; i<n ; i++){
+            System.out.print("Element " + i + ": ");
+            System.out.println(array[i]);
+        }
+
+        //Sum of elements
+        System.out.print("Calculating sum of elements... \n");
+        int sum = 0;
+        for (int i = 0 ; i<n ; i++){
+            sum += array[i];
+            if (i == n-1)
+                System.out.print("Sum is: " + sum);
+        }
+
+        //Average of elements
+        System.out.print("\nCalculating average of elements... \n");
+        double avg = (double) sum / n;
+        System.out.printf("Average is %.2f: " , avg);
+
+        //Minimum element
+        System.out.print("\nFinding minimum element... \n");
+        int min = array[0];
+        for (int i = 1 ; i<n ; i++){
+            if (array[i] < min)
+                min = array[i];
+        }
+        System.out.print("Minimum element is: " + min);
+
+        //Maximum element
+        System.out.print("\nFinding maximum element... \n");
+        int max = array[0];
+        for (int i = 1 ; i<n ; i++){
+            if (array[i] > max)
+                max = array[i];
+        }
+        System.out.print("Maximum element is: " + max);
+        sc.close();
     }
-    
 }
