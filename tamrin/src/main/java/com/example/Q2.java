d3b16f4ae2c5f8015ebe63355be379479a39b18f
kiiianaaa <kianakhorasani6@gmail.com>
2025-10-15 18:12:02 +0330

    Q2 done.

--- tamrin/src/main/java/com/example/Q2.java
+++ tamrin/src/main/java/com/example/Q2.java
@@ -1,5 +1,44 @@
 package com.example;
+import java.util.Scanner;
 
 public class Q2 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char c = sc.next().charAt(0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print(c);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i + j == n + 1) {
+                    System.out.print(c);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
