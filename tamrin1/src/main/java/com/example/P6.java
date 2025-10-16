43220ba5961bc09f7ce261d0d2471881f4d82552
farhadhosseinitalikarani <sydfarhadhosseini80@gmail.com>
2025-10-15 20:55:46 -0700

    P6 done.

---
+++ tamrin1/src/main/java/com/example/P6.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class P6 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter size = ");
+        int n = input.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = input.nextInt();
+        }
+        sort(a, n);
+        System.out.print("enter key:");
+        int key = input.nextInt();
+        lsearch(a, n, key);
+
+        show(a, n);
+
+    }
+
+    public static void sort(int a[], int s) {
+        for (int i = 0; i < s - 1; i++) {
+            for (int j = 0; j < s - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+
+                }
+            }
+        }
+    }
+
+    public static void show(int a[], int b) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.format("%d ", a[i]);
+
+        }
+        System.out.println();
+    }
+
+    public static void lsearch(int a[], int c, int key) {
+        boolean u = false;
+        int z = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == key) {
+                z = i;
+                u = true;
+
+            }
+
+        }
+        if (u) {
+            System.out.format("founded in %d\n", z);
+
+        } else {
+            System.out.println("not founded\n");
+
+        }
+
+    }
+
+}
