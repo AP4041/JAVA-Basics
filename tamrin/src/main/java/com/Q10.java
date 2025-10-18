75775e7b8d264cd147ea799b51b42a826a38343d
shaqayeqkhk <shaghayeghkhoobkar@gmail.com>
2025-10-18 12:44:20 -0700

    Q10 done.

---
+++ tamrin/src/main/java/com/Q10.java
@@ -0,0 +1,32 @@
+package com;
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int [][] table = new int[n][n];
+        int mainDiagonalSum =0;
+        int secondaryDiagonalSum =0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                table[i][j] = (i+1) * (j+1);
+                System.out.print(table[i][j] + " ");
+                if(i == j){
+                    mainDiagonalSum += table[i][j];
+                }
+                if(i + j == n - 1){
+                    secondaryDiagonalSum += table[i][j];
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("mainDiagonalSum= " + mainDiagonalSum);
+        System.out.println("secondaryDiagonalSume= " + secondaryDiagonalSum);
+
+        sc.close();
+    }
+    
+}
